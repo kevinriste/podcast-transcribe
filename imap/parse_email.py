0c93e8374dca72dda8684338e11e05861f715476
Kevin Riste <kevinbobriste@gmail.com>
2021-12-14 22:53:55 +0000

    add ability to send single URL as email to service

--- imap/parse_email.py
+++ imap/parse_email.py
@@ -1,6 +1,10 @@
 import os
 import re
 import operator
+import requests
+from trafilatura import fetch_url, extract
+
+from bs4 import BeautifulSoup
 
 from imap_tools import MailBox, A, AND, OR, NOT, MailMessageFlags
 
@@ -18,15 +22,30 @@ with MailBox('imap.gmail.com').login(gmail_user, gmail_password) as mailbox:
         clean_from = re.sub(r'[^A-Za-z0-9 ]+', '', from_)
         clean_from = clean_from + '- ' if clean_from != '' else ''
         clean_subject = re.sub(r'[^A-Za-z0-9 ]+', '', subject)
-        output_filename = f'{output_folder}/{date}-{clean_from}{clean_subject}.txt'
-        print(f'parsing email: {output_filename}')
-        email_text = msg.text
-        first_clean_email_text = re.sub(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', '', email_text)
-        second_clean_email_text = re.sub(r'\[\]', '', first_clean_email_text)
-        third_clean_email_text = re.sub(r'\(\)', '', second_clean_email_text)
-        clean_email_text = re.sub(r'<>', '', third_clean_email_text)
-        output_file = open(output_filename,"w")
-        output_file.write(clean_email_text)
-        output_file.close()
+        if clean_subject != 'link':
+            output_filename = f'{output_folder}/{date}-{clean_from}{clean_subject}.txt'
+            print(f'parsing email: {output_filename}')
+            email_text = msg.text
+            first_clean_email_text = re.sub(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', '', email_text)
+            second_clean_email_text = re.sub(r'\[\]', '', first_clean_email_text)
+            third_clean_email_text = re.sub(r'\(\)', '', second_clean_email_text)
+            clean_email_text = re.sub(r'<>', '', third_clean_email_text)
+            output_file = open(output_filename,"w")
+            output_file.write(clean_email_text)
+            output_file.close()
+        else:
+            email_text = msg.text
+            email_text = re.sub(r'[^\S]+', '', email_text)
+            print(f'fetching webpage: {email_text}')
+            # Make a GET request to fetch the raw HTML content using URL which should be entire body of webpage
+            html_content_for_soup = requests.get(email_text).text
+            soup = BeautifulSoup(html_content_for_soup, "html.parser")
+            html_content = fetch_url(email_text)
+            webpage_text = extract(html_content, include_comments=False)
+            output_filename = f'{output_folder}/{date}-{soup.title.string}.txt'
+            # webpage_text = soup.get_text()
+            output_file = open(output_filename,"w")
+            output_file.write(webpage_text)
+            output_file.close()
         flags = (MailMessageFlags.SEEN)
         mailbox.flag(msg.uid, flags, True)
