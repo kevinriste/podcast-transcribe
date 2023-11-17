6eadc6770fc198c16174085d17bf3a834fb25ce0
Kevin Riste <kevinbobriste@gmail.com>
2023-11-17 01:39:19 +0000

    find_duplicates version 3

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,18 +1,21 @@
 import os
+from collections import defaultdict
 
 
 def find_duplicate_filenames_with_size(directory):
-    seen = {}
+    seen = defaultdict(list)
     for filename in os.listdir(directory):
         full_path = os.path.join(directory, filename)
         if os.path.isfile(full_path):
             size = os.path.getsize(full_path)
             name = os.path.splitext(filename)[0][:-8]
-            if name in seen:
-                if seen[name] == size:
-                    print(f"Duplicate: {filename} (Size: {size} bytes)")
-            else:
-                seen[name] = size
+            seen[name].append((filename, size))
+
+    for name, file_list in seen.items():
+        if len(file_list) > 1:
+            print(f"Matching pairs for '{name}':")
+            for file, size in file_list:
+                print(f"File: {file} (Size: {size} bytes)")
 
 
 if __name__ == "__main__":
