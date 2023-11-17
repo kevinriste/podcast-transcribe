8a8296c22bd5347113bd4ddd1fb927764f537680
Kevin Riste <kevinbobriste@gmail.com>
2023-11-17 01:37:10 +0000

    second find_duplicates script

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,16 +1,20 @@
 import os
 
 
-def find_duplicate_filenames(directory):
+def find_duplicate_filenames_with_size(directory):
     seen = {}
     for filename in os.listdir(directory):
-        name = os.path.splitext(filename)[0][:-8]
-        if name in seen:
-            print(filename)
-        else:
-            seen[name] = None
+        full_path = os.path.join(directory, filename)
+        if os.path.isfile(full_path):
+            size = os.path.getsize(full_path)
+            name = os.path.splitext(filename)[0][:-8]
+            if name in seen:
+                if seen[name] == size:
+                    print(f"Duplicate: {filename} (Size: {size} bytes)")
+            else:
+                seen[name] = size
 
 
 if __name__ == "__main__":
-    directory_path = "dropcaster-docker/audio"  # Replace with your directory path
-    find_duplicate_filenames(directory_path)
+    directory_path = "dropcaster-docker/audio"
+    find_duplicate_filenames_with_size(directory_path)
