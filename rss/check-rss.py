1eb5c43eef17a37d93eafedcd3c9a411fb11a1be
Kevin Riste <kevinbobriste@gmail.com>
2023-12-01 16:30:42 +0000

    Add Tom Friedman to RSS

--- rss/check-rss.py
+++ rss/check-rss.py
@@ -23,6 +23,7 @@ wayback_feeds = [
     "https://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/column/ross-douthat/rss.xml",
     "https://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/column/paul-krugman/rss.xml",
     "https://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/column/ezra-klein/rss.xml",
+    "https://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/column/thomas-l-friedman/rss.xml",
 ]
 
 feeds = [line.rstrip() for line in open(feedsFile)]
